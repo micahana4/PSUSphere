cfee01ba9ab795d0b054e76d4008810929118c53
cojjeeee <gi686273@gmail.com>
2026-08-31 09:16:56 +0800

    added forms.py and add organization feature

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from studentorg.models import Organization
-class HomePageView(ListView):
+from studentorg.forms import OrganizationForm
+from django.urls import reverse_lazy
+
+class HomePageView(ListView):   
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
@@ -11,5 +15,11 @@ class OrganizationList(ListView) :
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+
+class OrganizationCreateView(CreateView) :
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_form.html'
+    success_url = reverse_lazy('organization-list')
 
 # Create your views here.s
